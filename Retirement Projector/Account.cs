c81b5f9c6895f9f0eaf5e3972c361217386659cc
StyxUT <StyxUT@gmail.com>
2014-08-28 20:36:57 -0600

    add .gitignore; add Accounts base class

---
+++ Retirement Projector/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retirement_Projector
+{
+    abstract class Account
+    {
+
+        private double preRetirementValue;
+        private double postRetirementValue;
+        private string accountName;
+
+	    public string AccountName
+	    {
+		    get { return accountName;}
+		    private set { accountName = value;}
+	    }
+	
+	    public double PostRetirementValue
+	    {
+		    get { return postRetirementValue;}
+		    private set { postRetirementValue = value;}
+	    }
+
+	    public double PreRetirementValue
+	    {
+		    get { return preRetirementValue;}
+		    private set { preRetirementValue = value;}
+	    }
+		
+        
+        public virtual void AddMonthlyContribution(int ContributionAmount)
+        {
+            PreRetirementValue += ContributionAmount;
+        }
+
+        public virtual void AddMonthlyROI(double ReturnRate)
+        {
+            PreRetirementValue += PreRetirementValue * ReturnRate;
+            PostRetirementValue += PostRetirementValue * ReturnRate;
+        }
+
+        public virtual void DeductMonthlyExpenses(int MonthlyExpense)
+        {
+            PostRetirementValue -= MonthlyExpense;
+        }
+
+        public virtual void OutputStatus()
+        { 
+        }
+    }
+}
